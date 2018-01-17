957df4a6c423915babe6fafa11768785d00a4a9a
eigerkl@gmail.com <kuaile2>
2018-01-17 17:15:46 +0800

    jquery  click/mouseenter

--- main.js
+++ main.js
@@ -1,28 +1,52 @@
+
+
+ //jquery
+ //
+ //
+ //点击
+     $('.tabt').click(function(){
+         var index = $(this).index();
+         $(".tabc").eq(index).show().siblings().hide();
+     })
+
+// 鼠标移入
+    $('.tabt').mouseenter(function(){
+        var index = $(this).index();
+        $('tabc').eq(index).show().siblings().hide();
+    })
+
+
 /**
- * Created by Administrator on 2017/8/25 0025.
+ * 原生js
  */
 
-    var title=document.getElementsByClassName('title');
-    var title_son=document.getElementsByClassName('title_son');
-    var con=document.getElementsByClassName('con');
-    var con_son=document.getElementsByClassName('con_son');
-    console.log(title,title_son,con,con_son)
-    for(let i=0;i<title_son.length;i++) {
-        title_son[i].index = i;
-        title_son[i].onclick = function () {
-            for (let j = 0; j < title_son.length; j++) {
-                con_son[j].style.zIndex = 1;
+    var tabts=document.getElementsByClassName('tab-title');
+    var tabt=document.getElementsByClassName('tabt');
+    var tabcons=document.getElementsByClassName('tab-con');
+    var tabc=document.getElementsByClassName('tabc');
+
+
+/**
+ * 层级
+ */
+    for(let i=0;i<tabt.length;i++) {
+        tabt[i].index = i;
+        tabt[i].onclick = function () {
+            for (let j = 0; j < tabt.length; j++) {
+                tabc[j].style.zIndex = 1;
             }
-            con_son[this.index].style.zIndex = 10;
+            tabc[this.index].style.zIndex = 10;
         }
     }
-
-
-
-
-
-
-
-
-
-
+/**
+ * 隐藏
+ */
+     for(let i=0;i<tabt.length;i++) {
+         tabt[i].index = i;
+         tabt[i].onclick = function () {
+             for (let j = 0; j < tabt.length; j++) {
+                 tabc[j].style.display = "none";
+             }
+             tabc[this.index].style.display = "block;";
+         }
+     }
